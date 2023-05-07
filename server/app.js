0290fa21f6cdbc0aea4cb6df0fd4fd5878447978
SkibstedN <niko933n@stud.kea.dk>
2023-05-07 21:06:54 +0200

    Session edit

--- server/app.js
+++ server/app.js
@@ -1,9 +1,22 @@
 import express from "express";
 import dotenv from 'dotenv';
 import mongoose from 'mongoose';
+import session from 'express-session';
+import bcrypt from 'bcrypt';
+
 
 const app = express();
 dotenv.config();
+
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      maxAge: 60 * 60 * 1000, // 1 hour
+    },
+  }));
+  
 
 const connectionString = process.env.MONGODB_CONNECT;
 
