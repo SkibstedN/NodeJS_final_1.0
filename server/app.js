1e6edda22c1c863881c8abaab85a6fab6bc8aa85
SkibstedN <niko933n@stud.kea.dk>
2023-05-07 17:00:22 +0200

    Created MongoDB Atlas connection

--- server/app.js
+++ server/app.js
@@ -1,3 +1,14 @@
 import express from "express";
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
 
 const app = express();
+dotenv.config();
+
+const connectionString = process.env.MONGODB_CONNECT;
+
+mongoose.connect(connectionString, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Connected to MongoDB Atlas'))
+  .catch(err => console.error('Could not connect to MongoDB Atlas', err));
+
+
