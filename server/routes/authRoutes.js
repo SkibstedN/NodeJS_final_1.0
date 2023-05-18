8fbc8aebe7da9a272433eec2b7786cacbfc57a92
SkibstedN <niko933n@stud.kea.dk>
2023-05-18 11:49:24 +0200

    nodemailer configuration

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,8 +1,30 @@
 import express from 'express';
 import User from '../models/User.js';
 import nodemailer from 'nodemailer';
+import crypto from 'crypto';
 
 const router = express.Router();
+
+const sendEmail = async (options) => {
+  const transporter = nodemailer.createTransport({
+    host: 'mail.gmx.com',
+    port: 587,
+    secure: false,
+    auth: {
+      user: process.env.EMAIL_ADDRESS,
+      pass: process.env.EMAIL_PASSWORD,
+    },
+  });
+
+  const mailOptions = {
+    to: options.to,
+    from: process.env.EMAIL_ADDRESS,
+    subject: options.subject,
+    text: options.text,
+  };
+
+  await transporter.sendMail(mailOptions);
+}
 
 router.post('/register', async (req, res) => {
   const { username, email, password } = req.body;
@@ -11,6 +33,14 @@ router.post('/register', async (req, res) => {
     const user = new User({ username, email, password });
     await user.save();
     req.session.userId = user._id;
+
+    // Call sendEmail function
+    await sendEmail({
+      to: user.email,
+      subject: 'Welcome to Our Application',
+      text: `Dear ${username},\n\nWelcome to our application. We are glad to have you onboard.\n`,
+    });
+
     res.status(201).json({ message: 'User registered successfully' });
   } catch (error) {
     res.status(500).json({ error: error.message });
@@ -35,6 +65,53 @@ router.post('/login', async (req, res) => {
     res.status(500).json({ error: error.message });
   }
 });
+
+router.post('/forgotPassword', async (req, res) => {
+  try {
+    const user = await User.findOne({ email: req.body.email });
+    if (!user) return res.status(404).json({ error: 'User not found' });
+
+    const token = crypto.randomBytes(20).toString('hex');
+
+    user.resetPasswordToken = token;
+    user.resetPasswordExpires = Date.now() + 3600000;
+
+    await user.save();
+
+    // Call sendEmail function
+    await sendEmail({
+      to: user.email,
+      subject: 'Password Reset Request',
+      text: `You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n
+              Please click on the following link, or paste this into your browser to complete the process within one hour of receiving it:\n\n
+              http://localhost:${process.env.PORT}/auth/resetPassword/${token}\n\n
+              If you did not request this, please ignore this email and your password will remain unchanged.\n`,
+    });
+
+    res.status(200).json('recovery email sent');
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+router.post('/resetPassword/:token', async (req, res) => {
+  const { password } = req.body;
+  try {
+    const user = await User.findOne({ resetPasswordToken: req.params.token, resetPasswordExpires: { $gt: Date.now() } });
+    if (!user) {
+      return res.status(404).json({ error: 'Password reset token is invalid or has expired.' });
+    }
+
+    user.password = password;
+    user.resetPasswordToken = undefined;
+    user.resetPasswordExpires = undefined;
+    await user.save();
+    res.json({ message: 'Password has been reset successfully.' });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
 
 router.post('/logout', (req, res) => {
   if (req.session) {
