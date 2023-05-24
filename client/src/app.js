9defbe5b530e4fe2a7952b79ad3f0827691ac645
SkibstedN <niko933n@stud.kea.dk>
2023-05-24 11:46:30 +0200

    Frontend interacting with Backend - test: Success

--- client/src/app.js
+++ client/src/app.js
@@ -7,7 +7,41 @@ document.addEventListener('DOMContentLoaded', function() {
     if (loginForm) {
       loginForm.addEventListener('submit', (e) => {
         e.preventDefault();
-        // TODO: Fetch form inputs and send a request to the server
+    
+        const emailInput = document.getElementById('login-email');
+        const passwordInput = document.getElementById('login-password');
+    
+        fetch('http://localhost:5000/auth/login', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({
+            email: emailInput.value,
+            password: passwordInput.value
+          })
+        })
+        .then(response => response.json())
+        .then(data => {
+          if (data.error) {
+            // Show error message
+            toastr.error(data.error);
+          } else {
+            // Show success message and clear the form
+            toastr.success('Logged in successfully!');
+            loginForm.reset();
+
+            setTimeout(() => {
+              window.location.href = 'frontpage.html';
+            }, 3000); 
+
+          }
+        })
+        .catch(error => {
+          console.error('Error:', error);
+          // Show error message
+          toastr.error('Something went wrong, please try again later.');
+        });
       });
     }
   
--- server/app.js
+++ server/app.js
@@ -1,10 +1,12 @@
 import express from "express";
+import cors from "cors";
 import dotenv from 'dotenv';
 import mongoose from 'mongoose';
 import session from 'express-session';
 import authRoutes from './routes/authRoutes.js';
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 dotenv.config();
 
