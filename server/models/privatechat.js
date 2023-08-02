3314b0d68cee877152a137766b534309d2f290ca
SkibstedN <niko933n@stud.kea.dk>
2023-08-02 18:21:39 +0200

    Update privatechat.js

--- server/models/privatechat.js
+++ server/models/privatechat.js
@@ -1,16 +1,22 @@
-import mongoose from 'mongoose';
-import { v4 as uuidv4 } from 'uuid';
+import mongoose from "mongoose";
+import { v4 as uuidv4 } from "uuid";
 
 const PrivateChatSchema = new mongoose.Schema({
-    roomId: { type: String, default: uuidv4, unique: true }, 
-    user: {
-      username: { type: String, required: true }
+  roomId: { type: String, default: uuidv4, unique: true },
+  user: {
+    username: { type: String, required: true },
+  },
+  otherUser: {
+    username: { type: String, required: true },
+  },
+  messages: [
+    {
+      sender: String,
+      content: String,
+      sentOn: { type: Date, default: Date.now },
     },
-    otherUser: {
-      username: { type: String, required: true }
-    },
-    messages: [{ sender: String, content: String, sentOn: { type: Date, default: Date.now } }],
-  });
+  ],
+});
 
-const privatechat = mongoose.model('privatechat', PrivateChatSchema);
+const privatechat = mongoose.model("privatechat", PrivateChatSchema);
 export default privatechat;
